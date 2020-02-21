ab8ed03d3e3dfe7a9779e33026f24005ffffadee
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-02-20 20:55:17 -0500

    home page login link works

--- conf/settings.py
+++ conf/settings.py
@@ -128,5 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = ''
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
